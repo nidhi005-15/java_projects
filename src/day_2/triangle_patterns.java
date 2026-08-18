efeacd2611b54a0039c451957affb25f4eb5e976
nidhiaathreya <aathreyanidhi@gmail.com>
2026-08-18 13:18:47 +0530

    added more programs

---
+++ src/day_2/triangle_patterns.java
@@ -0,0 +1,17 @@
+package day_2;
+
+public class triangle_patterns {
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
