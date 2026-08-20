ccc3ad99482df15fbb93e14bb54c27f08c10a2f6
nidhiaathreya <aathreyanidhi@gmail.com>
2026-08-20 14:47:52 +0530

    program on modulo operator

---
+++ src/day_4/modulo_operator.java
@@ -0,0 +1,14 @@
+package day_4;
+public class modulo_operator {
+public static void main(String[] args) {
+	int n=10;
+	int remainder=n%2;
+	System.out.println("remainder:"+remainder);
+	if(remainder==0) {
+		System.out.println("number is even");
+	}
+	else {
+		System.out.println("number is odd");
+	}
+}
+}
