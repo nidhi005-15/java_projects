6e11a8e7fbf2577a8eaeac09fc4ee326073e6da4
nidhiaathreya <aathreyanidhi@gmail.com>
2026-08-20 14:58:11 +0530

    simple usage of modulo operator

---
+++ src/day_4/modulo_operator_2.java
@@ -0,0 +1,10 @@
+package day_4;
+
+public class modulo_operator_2 {
+	public static void main(String[] args) {
+		int students=23;
+		int students_per_row=5;
+		System.out.println("students left after occupying:"+(students%students_per_row));
+	}
+
+}
