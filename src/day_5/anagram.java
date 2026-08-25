13d546f8dffc26f2f06adaee5dcd3785b824a93d
nidhiaathreya <aathreyanidhi@gmail.com>
2026-08-25 08:49:58 +0530

    updation on anagram

--- src/day_5/anagram.java
+++ src/day_5/anagram.java
@@ -1,17 +1,23 @@
+//checking for length
+//case sensitive
+//listen becomes silent
 package day_5;
-import java.util.random.*;
 import java.util.*;
 public class anagram {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter a string:");
-		String s=sc.next();
-		String x=random(s);
-		
-		
-		
-		
-		
+		String s1=sc.nextLine();
+		System.out.println("enter a string:");
+		String s2=sc.nextLine();
+		char[] a=s1.toCharArray();
+		char[] b=s2.toCharArray();
+		Arrays.sort(a);
+		Arrays.sort(b);
+		if(Arrays.equals(a,b)) {
+			System.out.println("anagram");
+		}else {
+			System.out.println("not anagram");
+		}
 	}
-
 }
