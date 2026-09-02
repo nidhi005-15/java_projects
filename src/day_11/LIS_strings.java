51265af8581f93d6d99c551222026a38544ce88c
nidhiaathreya <aathreyanidhi@gmail.com>
2026-09-02 11:37:29 +0530

    longest increasing subsequence

---
+++ src/day_11/LIS_strings.java
@@ -0,0 +1,24 @@
+package day_11;
+
+public class LIS_strings {
+	 public static void main(String[] args) {
+			char[] arr= {'a','e','b','c','d','e'};
+			int n=arr.length;
+			int [] dp=new int[n];
+			int max=0;
+			for(int i=0;i<n;i++) {
+				dp[i]=1;
+				for(int j=0;j<i;j++) {
+					if(arr[j]<arr[i]) {
+						dp[i]=Math.max(dp[i],dp[j]+1);
+					}
+				}
+				max=Math.max(max, dp[i]);
+			}
+			System.out.println("LTS Length = "+max);
+		}
+
+	}
+
+
+
