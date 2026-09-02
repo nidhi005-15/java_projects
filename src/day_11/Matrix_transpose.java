cad372350987774d99a0e8b75f640dbbd68902a8
nidhiaathreya <aathreyanidhi@gmail.com>
2026-09-02 14:50:17 +0530

    matrix transpose

---
+++ src/day_11/Matrix_transpose.java
@@ -0,0 +1,15 @@
+package day_11;
+
+public class Matrix_transpose {
+	public static void main(String[] args) {
+		int[][]matrix= {{1,2,3},{4,5,6}};
+		System.out.println("Transpose:");
+		for(int j=0;j<3;j++) {
+			for(int i=0;i<2;i++) {
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
