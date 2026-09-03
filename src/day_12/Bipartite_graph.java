a52b906820dd1b75708ceb3d2845d6168f017eb5
nidhiaathreya <aathreyanidhi@gmail.com>
2026-09-03 11:35:30 +0530

    bipartite graph

---
+++ src/day_12/Bipartite_graph.java
@@ -0,0 +1,42 @@
+package day_12;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Bipartite_graph {
+	static boolean isbipartite(int [][]graph) {
+		int n=graph.length;
+		int [] color=new int[n];
+		Arrays.fill(color,-1);
+		Queue<Integer> queue =new LinkedList<>();
+		for(int start=0;start<n;start++) {
+			if(color[start]!=-1) 
+				continue;
+			color[start]=0;
+			queue.add(start);
+			while(!queue.isEmpty()) {
+				int current=queue.poll();
+				for(int next:graph[current]) {
+					if(color[next]==-1) {
+						color[next]=1-color[current];
+						queue.add(next);
+					}
+					else if(color[next]==color[current]) {
+						return false;
+					}
+				}
+				
+			}
+		}
+		return true;
+		
+	}
+	public static void main(String[] args) {
+		int[][]graph= {{1,2},{0,3},{0,3},{1,2}};
+		System.out.println(isbipartite(graph));
+		
+		}
+	}
+
+
